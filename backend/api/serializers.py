3fa840211483883db11c5223b541fad141936435
Denis Kirichenko <michaelkeehl1@yandex.ru>
2022-02-06 20:13:20 +0100

    First deploy

--- backend/api/serializers.py
+++ backend/api/serializers.py
@@ -2,6 +2,7 @@ from django.shortcuts import get_object_or_404
 from rest_framework import serializers
 from rest_framework.validators import UniqueTogetherValidator
 from users.models import Follow
+
 from foodgram.models import (Cart, Favorite, Ingredient, Recipe,
                              RecipeIngredient, Tag)
 
