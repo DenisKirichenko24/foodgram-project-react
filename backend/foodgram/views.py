4b7cf6e3e0035096716d8738c28305c6e1d28505
Denis Kirichenko <michaelkeehl1@yandex.ru>
2022-02-07 21:21:14 +0100

    Fix subscribe

--- backend/foodgram/views.py
+++ backend/foodgram/views.py
@@ -3,15 +3,20 @@ from api.pagination import LimitPageNumberPagination
 from api.permissions import IsAdminOrReadOnly, IsOwnerOrReadOnly
 from api.serializers import (CropRecipeSerializer, IngredientSerializer,
                              RecipeSerializer, TagSerializer)
+from django.db.models import Sum
+from django.http import HttpResponse
 from django.shortcuts import get_object_or_404
 from django_filters.rest_framework import DjangoFilterBackend
+from reportlab.pdfbase import pdfmetrics
+from reportlab.pdfbase.ttfonts import TTFont
+from reportlab.pdfgen.canvas import Canvas
 from rest_framework import status, viewsets
 from rest_framework.decorators import action
 from rest_framework.permissions import IsAuthenticated
 from rest_framework.response import Response
 from rest_framework.viewsets import ReadOnlyModelViewSet
 
-from .models import Cart, Favorite, Ingredient, Recipe, Tag
+from .models import Cart, Favorite, Ingredient, Recipe, RecipeIngredient, Tag
 
 
 class TagsViewSet(ReadOnlyModelViewSet):
@@ -39,23 +44,57 @@ class RecipeViewSet(viewsets.ModelViewSet):
     def perform_create(self, serializer):
         return serializer.save(author=self.request.user)
 
-    @action(detail=True, methods=['get', 'delete'],
+    @action(detail=True, methods=['post', 'delete'],
             permission_classes=[IsAuthenticated])
     def favorite(self, request, pk=None):
-        if request.method == 'GET':
+        if request.method == 'POST':
             return self.add_obj(Favorite, request.user, pk)
         if request.method == 'DELETE':
             return self.delete_obj(Favorite, request.user, pk)
         return None
 
-    @action(detail=True, methods=['get', 'delete'],
+    @action(detail=True, methods=['post', 'delete'],
             permission_classes=[IsAuthenticated])
     def shopping_cart(self, request, pk=None):
-        if request.method == 'GET':
+        if request.method == 'POST':
             return self.add_obj(Cart, request.user, pk)
         if request.method == 'DELETE':
             return self.delete_obj(Cart, request.user, pk)
         return None
+
+    def generate_pdf_shopping_list(self, user):
+        shopping_list = RecipeIngredient.objects.filter(
+            recipe__cart__user=user).values(
+                'ingredient__name',
+                'ingredient__measurement_unit'
+        ).annotate(amount=Sum('amount')).order_by()
+        response = HttpResponse(content_type='application/pdf')
+        response['Content-Disposition'] = (
+            'attachment; filename="shopping_list.pdf"'
+        )
+        pdfmetrics.registerFont(
+            TTFont('DejaVuSerif', 'DejaVuSerif.ttf', 'UTF-8')
+        )
+        page = Canvas(filename=response)
+        page.setFont('DejaVuSerif', 24)
+        page.drawString(210, 800, 'Список покупок')
+        page.setFont('DejaVuSerif', 16)
+        height = 760
+        is_page_done = False
+        for idx, ingr in enumerate(shopping_list, start=1):
+            is_page_done = False
+            page.drawString(60, height, text=(
+                f'{idx}. {ingr["ingredient__name"]} - {ingr["amount"]} '
+                f'{ingr["ingredient__measurement_unit"]}'
+            ))
+            height -= 30
+            if height <= 40:
+                page.showPage()
+                is_page_done = True
+        if not is_page_done:
+            page.showPage()
+        page.save()
+        return response
 
     def add_obj(self, model, user, pk):
         if model.objects.filter(user=user, recipe__id=pk).exists():
