b6e85dfa0592817b78016f65c2ea33f8573fef8b
Denis Kirichenko <michaelkeehl1@yandex.ru>
2022-02-08 18:59:55 +0100

    Fix favorite

--- backend/foodgram/views.py
+++ backend/foodgram/views.py
@@ -53,14 +53,14 @@ class RecipeViewSet(viewsets.ModelViewSet):
             return self.delete_obj(Favorite, request.user, pk)
         return None
 
-    @action(detail=True, methods=['get', 'delete'],
+    @action(detail=True, methods=['post', 'delete'],
             permission_classes=[IsAuthenticated])
     def shopping_cart(self, request, pk=None):
-        if request.method == 'GET':
+        if request.method == 'POST':
             return self.add_obj(Cart, request.user, pk)
         if request.method == 'DELETE':
             return self.delete_obj(Cart, request.user, pk)
-        return None
+        return Response(status=status.HTTP_200_OK)
 
     def generate_pdf_shopping_list(self, user):
         shopping_list = RecipeIngredient.objects.filter(
